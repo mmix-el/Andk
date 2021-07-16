88db616fef3df3fda764ac52ca668a3b66344824
mmix-el <mmix-el@yandex.ru>
2021-07-16 18:55:43 +0500

    Added logic to display the correct dice image based on the dice roll

--- app/src/main/java/xyz/mmixel/andk/MainActivity.kt
+++ app/src/main/java/xyz/mmixel/andk/MainActivity.kt
@@ -1,20 +1,19 @@
 package xyz.mmixel.andk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textView: TextView
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.result)
+        diceImage = findViewById(R.id.dice_image)
         findViewById<Button>(R.id.send_button).apply {
             setOnClickListener{
                 rollDice()
@@ -24,6 +23,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val resultInt: Int = (1..6).random()
-        textView.text = resultInt.toString()
+        val resourceDrawable = when(resultInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(resourceDrawable)
     }
 }
