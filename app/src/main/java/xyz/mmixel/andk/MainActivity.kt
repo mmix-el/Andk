165574d6fca42966b33ca37437a021e976a23e85
mmix-el <mmix-el@yandex.ru>
2021-07-16 18:38:18 +0500

    Added dummy business logic

--- app/src/main/java/xyz/mmixel/andk/MainActivity.kt
+++ app/src/main/java/xyz/mmixel/andk/MainActivity.kt
@@ -2,10 +2,27 @@ package xyz.mmixel.andk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.result)
+        findViewById<Button>(R.id.send_button).apply {
+            setOnClickListener{
+                rollDice()
+            }
+        }
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "The die is rolled!", Toast.LENGTH_SHORT).show()
     }
 }
